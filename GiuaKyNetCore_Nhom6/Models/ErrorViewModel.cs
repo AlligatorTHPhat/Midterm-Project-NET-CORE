c44cc785d092279116b79fa10acab1f8cf463702
Tran Hoang Phat <tranhoangphat23112005@gmail.com>
2026-04-28 08:09:54 +0700

    first commit

---
+++ GiuaKyNetCore_Nhom6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GiuaKyNetCore_Nhom6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
